4a704682b1f279ffa204a29b42130e024fa47f2c
ChinKok <chinkok1995@gmail.com>
2016-12-21 22:08:40 +0800

    ChinKok

--- CarGame.cpp
+++ CarGame.cpp
@@ -4,14 +4,18 @@
 
 using namespace std;
 
+#include "Framework/Test.h"
+#include "Framework/Render.h"
+
+#include "Car.h"
+
 int main()
 {
-	// For update frames
 	float fixedTimeStep = 0.02f;
 	sf::Clock fixedUpdateClock;
-	float timeElapsedForUpdateTime = 0;
-	
-	// window creation
+	float timeElapsedSinceLastFrame = 0;
+
+
 	int windowSizeX = 800, windowSizeY = 600;
 	int windowBorderSize = 16;
 	sf::RenderWindow window(sf::VideoMode(windowSizeX, windowSizeY), "Car");
@@ -19,18 +23,35 @@ int main()
 	// window.setVerticalSyncEnabled(true);
 	window.setActive();
 
+	b2Vec2 gravity(0.f, 9.81f);
+	b2World world(gravity);
+
+	sf::Vector2f horizontalBorderSize(windowSizeX,windowBorderSize);
+	sf::Vector2f verticalBorderSize(windowBorderSize,windowSizeY-windowBorderSize*2);
+	sf::Vector2f    topBorderPos(windowSizeX/2,windowBorderSize/2);
+	sf::Vector2f bottomBorderPos(windowSizeX/2,windowSizeY-windowBorderSize/2);
+	sf::Vector2f   leftBorderPos(windowBorderSize/2,windowSizeY/2);
+	sf::Vector2f  rightBorderPos(windowSizeX-windowBorderSize/2,windowSizeY/2);
+	
+	sf::Text text;
+	text.setCharacterSize(16);
+	text.setPosition(3, -3);
+	text.setColor(sf::Color::White);
 	while(window.isOpen())
 	{
 		sf::Event event;
+
 		while(window.pollEvent(event))
 		{
 			if(event.type == sf::Event::Closed)
 				window.close();
 		}
-
-
-		//chinkok
+		
 		window.clear(sf::Color(0,0,0));
+
+		text.setString("Testing For a Car");
+		window.draw(text);
+
 		window.display();
 	}
 	return 0;
