4cc769d222503fcf5b1328f519f5cdd649ff4d63
iceboy95 <hqkhor95@gmail.com>
2016-12-21 21:55:30 +0800

    modified something

--- CarGame.cpp
+++ CarGame.cpp
@@ -6,19 +6,31 @@ using namespace std;
 
 int main()
 {
+	// For update frames
+	float fixedTimeStep = 0.02f;
+	sf::Clock fixedUpdateClock;
+	float timeElapsedForUpdateTime = 0;
 	
-	
+	// window creation
 	int windowSizeX = 800, windowSizeY = 600;
 	int windowBorderSize = 16;
 	sf::RenderWindow window(sf::VideoMode(windowSizeX, windowSizeY), "Car");
 
-	window.setVerticalSyncEnabled(true);
+	// window.setVerticalSyncEnabled(true);
 	window.setActive();
 
 	while(window.isOpen())
 	{
+		sf::Event event;
+		while(window.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+				window.close();
+		}
+
+
 		//chinkok
-		window.clear(sf::Color(100, 149, 237));
+		window.clear(sf::Color(0,0,0));
 		window.display();
 	}
 	return 0;
