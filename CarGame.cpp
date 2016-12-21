e1f45a91cdd182c3a65297452005c7c1e769d4a1
iceboy95 <hqkhor95@gmail.com>
2016-12-21 20:29:40 +0800

    git init

---
+++ CarGame.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	
+	int windowSizeX = 800, windowSizeY = 600;
+	int windowBorderSize = 16;
+	sf::RenderWindow window(sf::VideoMode(windowSizeX, windowSizeY), "Car");
+
+	window.setVerticalSyncEnabled(true);
+	window.setActive();
+
+	while(window.isOpen())
+	{
+
+		window.clear(sf::Color(100, 149, 237));
+		window.display();
+	}
+	return 0;
+}
