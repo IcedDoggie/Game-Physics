63ee0558f4147043e04c210c6379d82521c5143c
iceboy95 <hqkhor95@gmail.com>
2016-12-27 22:26:32 +0800

    somehow solve the out of memory error

--- CarGame.cpp
+++ CarGame.cpp
@@ -64,12 +64,21 @@ int main()
 	// draw world
 	////1 -> tile, 2-> player 3->slope
 	int worldArray [15][20] = {
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		{2,0,3,0,0,0,0,0,0,0,0,0,0,0,0},
-		{1,1,1,1,1,1,0,1,1,1,1,1,1,1,1},
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{2,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}	
 	};
 
 	sf::VertexArray world(sf::Lines, 24);
@@ -85,6 +94,7 @@ int main()
 	world[6].position = sf::Vector2f(400,300);
 	world[7].position = sf::Vector2f(400,400);
 
+	//ending point
 	world[2].position = sf::Vector2f(500,400);
 	world[3].position = sf::Vector2f(800,400);
 
@@ -110,25 +120,63 @@ int main()
 				window.close();
 		}
 		// check if what is below the car is empty
-		for(int i=0; i<6; i++)
+		if(worldArray[current_position_car_x+1][current_position_car_y] == 0 && initial_velocity_right >0)
 		{
-			for(int j=0; j<8; j++)
+			cout<<current_position_car_x<<endl;
+			// update backend
+			worldArray[current_position_car_x][current_position_car_y] = 0;
+			current_position_car_x += 1;
+			worldArray[current_position_car_x][current_position_car_y] = 2;
+			for(int m=0; m<15; m++)
 			{
-				if(worldArray[i][j] == 2)
+				for(int n=0; n<20; n++)
 				{
-					if(worldArray[i+1][j] == 0)
-					{
-						// update backend array if car is dropping in terms of y-axis
-						gravity = 0.1f;
-						
-					} 
-					else
-					{
-						gravity = 0.0f;
-					}
+					// cout<<worldArray[m][n]<<" ";
 				}
-			}
+				// cout<<endl;
+			}	
+			cout<<endl;		
+			gravity = 0.01f;		
 		}
+
+		else if(worldArray[current_position_car_x+1][current_position_car_y] == 1)
+		{
+			gravity = 0.0f;
+		}
+
+
+
+
+		// for(int i=0; i<15; i++)
+		// {
+		// 	for(int j=0; j<20; j++)
+		// 	{
+		// 		if(worldArray[i][j] == 2)
+		// 		{
+		// 			if(worldArray[i+1][j] == 0)
+		// 			{
+		// 				cout<<"Falling...\n";
+		// 				// update backend array if car is dropping in terms of y-axis
+		// 				worldArray[current_position_car_x][current_position_car_y] = 0;
+		// 				current_position_car_x -= 1;
+		// 				worldArray[current_position_car_x + 1][current_position_car_y] = 2;
+		// 				gravity = 0.1f;	
+		// 			}
+
+		// 			else if(worldArray[i+1][j] == 3)
+		// 			{
+		// 				cout<<"On slope!\n";
+		// 				gravity = 0.0f;
+		// 			}
+
+		// 			else if(worldArray[i+1][j] == 1)
+		// 			{
+		// 				cout<<"no gravity!\n";
+		// 				gravity = 0.0f;
+		// 			}
+		// 		}
+		// 	}
+		// }
 
 		// let car go up slope
 		if(worldArray[current_position_car_x][current_position_car_y + 1] == 3 && initial_velocity_right > 0)
@@ -137,10 +185,10 @@ int main()
 			int initial_velocity_right_parsed = static_cast<int>(initial_velocity_right * 100);
 			int rotation_speed = calculateRotation(45, initial_velocity_right_parsed);
 			int rotation_car = calculateCarRotation(45, 200);
-			car_rect.rotate(-30);
-			// frontWheel.rotate(-30);
-			// backWheel.rotate(-30);
 			
+			// visualize
+			car_rect.rotate(-30);
+
 			// update world array
 			worldArray[current_position_car_x - 1][current_position_car_y + 1] = 2;
 			worldArray[current_position_car_x][current_position_car_y] = 0;
@@ -148,7 +196,6 @@ int main()
 			// update car position
 			current_position_car_x = current_position_car_x - 1;
 			current_position_car_y = current_position_car_y + 1;
-	
 		}
 
 		// for car to fall when there is no ground
@@ -165,7 +212,7 @@ int main()
 			initial_velocity_right += acceleration;
 
 			// move the car in the backend array
-			if(static_cast<int>(car_rect.getPosition().x) % 100 == 0 &&
+			if(static_cast<int>(car_rect.getPosition().x) % 20 == 0 &&
 			 worldArray[current_position_car_x][current_position_car_y+1] != 3)
 			{
 				worldArray[current_position_car_x][current_position_car_y] = 0;
@@ -180,7 +227,7 @@ int main()
 
 			car_rect.move(initial_velocity_left, 0);
 			initial_velocity_left -= acceleration;
-			if(static_cast<int>(car_rect.getPosition().x) % 100 == 0 &&
+			if(static_cast<int>(car_rect.getPosition().x) % 20 == 0 &&
 				worldArray[current_position_car_x-1][current_position_car_y] != 3)
 			{
 				worldArray[current_position_car_x][current_position_car_y] = 0;
@@ -192,18 +239,11 @@ int main()
 				cout<<"ping"<<endl;
 				worldArray[current_position_car_x][current_position_car_y] = 0;
 				current_position_car_y -= 1;
-				current_position_car_x += 1;
+				// current_position_car_x += 1;
 				worldArray[current_position_car_x][current_position_car_y] = 2;
 				car_rect.rotate(30);
 			}
-			for(int m=0; m<6; m++)
-			{
-				for(int n=0; n<8; n++)
-				{
-					cout<<worldArray[m][n]<<" ";
-				}
-				cout<<endl;
-			}		
+
 		}
 
 		// Rendering
